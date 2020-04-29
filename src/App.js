086f09e9ff292d00fbf9597e59abae8a815efd20
gmarinojr <garymarinojr@gmail.com>
2020-04-29 14:22:15 -0500

    created structure for merge data helper function

--- src/App.js
+++ src/App.js
@@ -2,68 +2,29 @@ import React, { useRef, useEffect } from 'react';
 import WebViewer from '@pdftron/pdfjs-express';
 import './App.css';
 import cft from './mockData.json';
-import {
-  xfdfString
-} from './xfdfDataString'
-const parsed = JSON.parse(cft);
+import { xfdfFields } from './xfdfFields';
+import { saveToXml } from './saveToXml';
 
 const App = () => {
   const viewer = useRef(null);
-  const input = document.getElementById('file_upload');
-  const saveBlob = document.getElementById('save_blob');
-  const loadBlob = document.getElementById('load_blob');
-
+  const mergedData = saveToXml(xfdfFields);
+  // const input = document.getElementById('file_upload');
+  // const saveBlob = document.getElementById('save_blob');
+  // const loadBlob = document.getElementById('load_blob');
   // let documentBlob;
-  let xfdfData;
-
+  // let xfdfData;
   // const saveBlobToServer = (blob) => {
   //   console.log(blob);
   // };
-
-  const viewer = useRef(null);
-  const saveBlobToServer = (blob) => {
-    console.log(blob);
-  };
   useEffect(() => {
-    WebViewer({
-      path: '/webviewer/lib',
-      initialDoc: '/files/VS 6-22.pdf',
-    },
+    WebViewer(
+      { path: '/webviewer/lib', initialDoc: '/files/VS 6-22.pdf' },
       viewer.current
     ).then((instance) => {
-      const {
-        docViewer
-      } = instance;
-      const annotManager = docViewer.getAnnotationManager();
-      input.addEventListener('change', function () {
-        var file = input.files[0];
-        instance.loadDocument(file, {
-          filename: file.name
-        });
-      });
+      const { docViewer, annotManager, Annotations } = instance;
       docViewer.on('annotationsLoaded', async () => {
-        const documentStream = await docViewer.getDocument().getFileData({});
-        documentBlob = new Blob([documentStream], {
-          type: 'application/pdf',
-        });
-        // console.log(typeof xfdfString, 'xfdf')
-        console.log(cft);
-        await annotManager.importAnnotations(xfdfString);
-      });
-      saveBlob.addEventListener('click', async () => {
-        xfdfData = await annotManager.exportAnnotations();
-        // saveBlobToServer(documentBlob);
-        console.log(xfdfData);
-      });
-      loadBlob.addEventListener('click', async () => {
-        // annotManager.importAnnotations(xfdfData);
-        await annotManager.importAnnotations(xfdfString);
-        // const getCommand = await annotManager.exportAnnotCommand();
-        // console.log(getCommand);
-        // instance.loadDocument(documentBlob, {
-        //   filename: 'myfile.pdf'
-        // });
-        // instance.drawAnnotations(xfdfData)
+        annotManager.importAnnotations(mergedData);
+        console.log(Annotations.total.getBottom())
       });
     });
   }, []);
@@ -77,3 +38,12 @@ const App = () => {
 };
 
 export default App;
+
+// saveBlob.addEventListener('click', async () => {
+//   xfdfData = await annotManager.exportAnnotations();
+//   console.log(xfdfData);
+//   saveBlobToServer(documentBlob);
+// });
+// loadBlob.addEventListener('click', async () => {
+//   await annotManager.importAnnotations(xfdfFields);
+// });
