92a6fe8df1cc878ca65e4f0cc360a851edcd0503
gmarinojr <garymarinojr@gmail.com>
2020-04-29 14:56:34 -0500

    removed console logs

--- src/mergeData.js
+++ src/mergeData.js
@@ -1,15 +1,4 @@
 export function mergeData(xml) {
-  // let txt = '';
-  // const path = '/xfdf/fields/field/1/name';
-  // if (xml.evaluate) {
-  //   var nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
-  //   var result = nodes.iterateNext();
-  //   while (result) {
-  //     txt += result.childNodes[0].nodeValue + '<br>';
-  //     result = nodes.iterateNext();
-  //   }
-  //   document.getElementById('demo').innerHTML = txt;
-  // }
   console.log(xml);
   return xml;
 }
