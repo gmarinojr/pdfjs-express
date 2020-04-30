ea03dfb83c3fc91d82af95066de2c87941171cd3
gmarinojr <garymarinojr@gmail.com>
2020-04-30 15:15:11 -0500

    created mapping function for testReason

--- src/helpers/cftRadioMappers.js
+++ src/helpers/cftRadioMappers.js
@@ -1,3 +1,27 @@
+export const reasonForTest = ({ testReason }) => {
+  if (testReason === 'Area') {
+    return 'id_1';
+  } else if (testReason === 'HerdAccreditation') {
+    return 'id_6';
+  } else if (testReason === 'MilkOrdinance') {
+    return 'id_7';
+  } else if (testReason === 'SaleShow') {
+    return 'id_8';
+  } else if (testReason === 'AffectedHerd') {
+    return 'id_9';
+  } else if (testReason === 'Retest') {
+    return 'id_10';
+  } else if (testReason === 'TracingRegKill') {
+    return 'id_11';
+  } else if (testReason === 'TracingReactors') {
+    return 'id_12';
+  } else if (testReason === 'TracingExposed') {
+    return 'id_13';
+  } else if (testReason === 'Other') {
+    return 'id_14';
+  }
+};
+
 export const kindOfHerd = ({ tuberculosisHerd }) => {
   if (tuberculosisHerd === 'Deer') {
     return 'id_3';
