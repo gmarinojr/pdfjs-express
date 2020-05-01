d063edbfc19cf3eee5e2a37030d95367d49a3a57
gmarinojr <garymarinojr@gmail.com>
2020-05-01 13:54:09 -0500

    removed console logs

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import { mergeData } from './mergeData';
 const App = () => {
   const viewer = useRef(null);
   const mergedData = mergeData(vs622Fields);
-  // const input = document.getElementById('file_upload');
-  const saveBlob = document.getElementById('save_blob');
+  // const saveBlob = document.getElementById('save_blob');
   // const loadBlob = document.getElementById('load_blob');
-  let xfdfData;
+  // let xfdfData;
 
   useEffect(() => {
     WebViewer(
@@ -20,15 +19,8 @@ const App = () => {
       const { docViewer, annotManager } = instance;
       var Feature = instance.Feature;
       instance.enableFeatures([Feature.Download]);
-      // console.log(instance.enableElement('downloadButton'), 'chbhcbfhb')
-      // instance.enableFeatures([Feature.FilePicker]);
       docViewer.on('annotationsLoaded', async () => {
-        // console.log(Feature.Download, 'heeey')
         annotManager.importAnnotations(mergedData);
-      });
-      saveBlob.addEventListener('click', async () => {
-        xfdfData = await annotManager.exportAnnotations();
-        console.log(xfdfData);
       });
     });
   }, []);
@@ -43,6 +35,10 @@ const App = () => {
 
 export default App;
 
+// saveBlob.addEventListener('click', async () => {
+//   xfdfData = await annotManager.exportAnnotations();
+//   console.log(xfdfData);
+// });
 // loadBlob.addEventListener('click', async () => {
 //   await annotManager.importAnnotations(vs622Fields);
 // });
